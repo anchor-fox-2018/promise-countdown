9d0c81b6e7de707919678f0211e02c9599243e59
anastasia <anastasia.santoso.id@gmail.com>
2018-09-21 07:56:15 +0700

    Release 0

--- controllers.js
+++ controllers.js
@@ -6,11 +6,11 @@ const timer = seconds =>
     const then = now + seconds * 1000;
 
     displayCountdown(seconds - 1);
-    setInterval(() => {
+    let time = setInterval(() => {
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
       if (secondsLeft < 0) {
-        clearInterval();
-        resolve();
+        clearInterval(time);
+        resolve(seconds);
       } else {
         displayCountdown(secondsLeft);
       }
