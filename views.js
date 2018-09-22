cf965c0ae7adc266f916bd2268be79da9b51499e
yayahidayatullah <yayahidayatullah@rocketmail.com>
2018-09-23 02:47:04 +0800

    done

--- views.js
+++ views.js
@@ -6,6 +6,7 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  console.log(chalk.red(figlet.textSync(String(seconds))))
 };
 
 module.exports = { displayCountdown };
